b4e61954c75e7ce4f11f5ce26c3fd2175d533768
tronguy <>
2023-06-14 19:15:33 -0300

    battery and firefox configs

---
+++ checkbattery.sh
@@ -0,0 +1,8 @@
+	export DISPLAY=:0
+	discharging=$(acpi -b | grep Discharging)
+	level=$(acpi -b | cut -f 2 -d ',' | cut -f 1 -d '%')
+	echo $level
+	if [[ $discharging && $level  -lt 17 ]]; then
+		dunstify -i battery "Bateria:$level, Coloque o notebook para carregar" --timeout=10000	
+	fi
+
